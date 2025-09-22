27e847b8479029c4736c3d8cc98958b0efa54c39
GiovanaSantoss <gigonzaga987@gmail.com>
2025-09-22 15:11:17 -0300

    Login required to register books

--- KitchenLab/recipes/views.py
+++ KitchenLab/recipes/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Recipe
+from .forms import RecipeForm
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/index.html', {'recipes': recipes})
+
+
+@login_required
+def add_recipe(request):
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe = form.save(commit=False)
+            recipe.created_by = request.user
+            recipe.save()
+            return redirect('index')
+    else:
+        form = RecipeForm()
+    return render(request, 'recipes/add_recipe.html', {'form': form})
