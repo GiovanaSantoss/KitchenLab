536121fccf0881fc23d6c4beb546037095903a97
GiovanaSantoss <gigonzaga987@gmail.com>
2025-09-22 15:47:47 -0300

    Delete botton

--- KitchenLab/recipes/urls.py
+++ KitchenLab/recipes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add_recipe, name='add_recipe'),
+    path('delete/<int:recipe_id>/', views.delete_recipe, name='delete_recipe'),
 ]
--- KitchenLab/recipes/views.py
+++ KitchenLab/recipes/views.py
@@ -3,6 +3,7 @@ from .models import Recipe
 from .forms import RecipeForm
 
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -22,3 +23,12 @@ def add_recipe(request):
     else:
         form = RecipeForm()
     return render(request, 'recipes/add_recipe.html', {'form': form})
+
+
+
+@login_required
+def delete_recipe(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    if recipe.created_by == request.user:  # só o dono pode deletar
+        recipe.delete()
+    return redirect('index')
