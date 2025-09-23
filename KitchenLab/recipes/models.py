e565abe87c9ce892582cf6c6f9c912b819fc2d48
GiovanaSantoss <gigonzaga987@gmail.com>
2025-09-23 14:02:44 -0300

    clickable recipe to show more details

--- KitchenLab/recipes/models.py
+++ KitchenLab/recipes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Recipe(models.Model):
     DIFFICULTY_CHOICES = [
         ('Easy', 'Easy'),
@@ -8,13 +9,26 @@ class Recipe(models.Model):
         ('Hard', 'Hard'),
     ]
 
+    CATEGORY_CHOICES = [
+        ('Breakfast', 'Breakfast'),
+        ('Lunch', 'Lunch'),
+        ('Dinner', 'Dinner'),
+        ('Comfort Food', 'Comfort Food'),
+        ('Quick and Easy', 'Quick and Easy'),
+        ('Low Carb', 'Low Carb'),
+        ('Salads', 'Salads'),
+        ('Desserts', 'Desserts'),
+        ('Drinks', 'Drinks'),
+    ]
+
     name = models.CharField(max_length=100)
     ingredients = models.TextField()
     instructions = models.TextField()
     prep_time = models.IntegerField(help_text="Preparation time in minutes")
     difficulty = models.CharField(max_length=20, choices=DIFFICULTY_CHOICES)
-    category = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='recipe_images/', null=True, blank=True)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    image = models.ImageField(
+        upload_to='recipe_images/', null=True, blank=True, verbose_name='Recipe Image')
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
